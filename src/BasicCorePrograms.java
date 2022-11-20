0198450865dcd335a5fe0843db484d069cd135fa
Prashik Kamble <kambleprashik7349@gmail.com>
2022-11-20 11:50:18 +0530

    ADDED : Code to find nth Harmonic value.

--- src/BasicCorePrograms.java
+++ src/BasicCorePrograms.java
@@ -7,10 +7,11 @@ public class BasicCorePrograms {
     static final int FLIP_COIN = 1;
     static final int CHECK_LEAP_YEAR = 2;
     static final int POWER_OF_TWO = 3;
+    static final int HARMONIC_NUMBER = 4;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Basic Core Programs!");
-        System.out.println("1. Find Heads or Tails/n2. Check Leap Year/3. Power of Two");
+        System.out.println("1. Find Heads or Tails\n2. Check Leap Year\n3. Power of Two\n4. Find Nth Harmonic Number");
         System.out.println("Enter which function you want to call : " );
 
         BasicCorePrograms bcpObj = new BasicCorePrograms();
@@ -22,6 +23,10 @@ public class BasicCorePrograms {
                 bcpObj.checkLeapYear();
             case POWER_OF_TWO ->
                 bcpObj.powerOFTwo();
+            case HARMONIC_NUMBER ->
+                bcpObj.findHarmonicValue();
+            default ->
+                    System.out.println("Wrong Choice");
         }
     }
 
@@ -78,6 +83,21 @@ public class BasicCorePrograms {
                 int table = (int) Math.pow(powerOf, i);
                 System.out.println("Power of Two is : " + table);
             }
+        }
+    }
+
+    public void findHarmonicValue () {
+        System.out.println("Enter number which you want to find Nth Harmonic number : ");
+        int userInput = scnr.nextInt();
+
+        if (userInput > 0) {
+            double harmonicNumber = 0;
+            for (double i = 1; i <= userInput; i++) {
+                harmonicNumber = harmonicNumber + (1 / i);
+            }
+            System.out.println(userInput + "th" + " Harmonic Value is : " + harmonicNumber);
+        } else {
+            System.out.println("Please enter positive numbers only.");
         }
     }
 }
