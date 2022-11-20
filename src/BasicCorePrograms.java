8a61012029f2eab2da14c3e99c41cb1e7b08a980
Prashik Kamble <kambleprashik7349@gmail.com>
2022-11-20 11:15:11 +0530

    ADDED : Code to get table of 2 upto certain condition.

--- src/BasicCorePrograms.java
+++ src/BasicCorePrograms.java
@@ -4,13 +4,25 @@ public class BasicCorePrograms {
     static Scanner scnr = new Scanner(System.in);
 
     static final float headOrTailCondition = 0.51f;
+    static final int FLIP_COIN = 1;
+    static final int CHECK_LEAP_YEAR = 2;
+    static final int POWER_OF_TWO = 3;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Basic Core Programs!");
+        System.out.println("1. Find Heads or Tails/n2. Check Leap Year/3. Power of Two");
+        System.out.println("Enter which function you want to call : " );
 
         BasicCorePrograms bcpObj = new BasicCorePrograms();
-        //bcpObj.isHead();
-        bcpObj.checkLeapYear();
+        int userChoice = scnr.nextInt();
+        switch (userChoice) {
+            case FLIP_COIN ->
+                bcpObj.isHead();
+            case CHECK_LEAP_YEAR ->
+                bcpObj.checkLeapYear();
+            case POWER_OF_TWO ->
+                bcpObj.powerOFTwo();
+        }
     }
 
     public void isHead () {
@@ -55,5 +67,17 @@ public class BasicCorePrograms {
                 System.out.println("Entered year i.e : " + year + " is Not a Leap year" );
         } else
             System.out.println("Please enter a 4 digit number!");
+    }
+
+    public void powerOFTwo() {
+        System.out.println("Enter number for power of 2 : ");
+        int userInput = scnr.nextInt();
+        int powerOf = 2;
+        for (int i = 1; i < userInput; i++) {
+            if (i < 31) {
+                int table = (int) Math.pow(powerOf, i);
+                System.out.println("Power of Two is : " + table);
+            }
+        }
     }
 }
